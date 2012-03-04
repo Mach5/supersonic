ca5d4e7bf67abdd8031acbb4cd0c4301feec1a8c
Timo Reimann <devel@foo-lounge.de>
2012-03-04 01:51:04 +0100

    Rebrand Debian package.
    Note: The changes allow for two scenarios to be implemented: Re-usage
    of the Subsonic configuration file and running Supersonic next to
    Subsonic without conflict.
    
    general:
    - Rename Subsonic to Supersonic where appropriate.
    
    subsonic-installer-debian:
    - Update group/artifact IDs.
    - Rename default configuration file and init script.
    
    subsonic-{booter,main}:
    - Update group/artifact IDs because it's a dependency for
      subsonic-installer-debian.

--- subsonic-booter/src/main/script/subsonic.bat
+++ subsonic-booter/src/main/script/subsonic.bat
@@ -1,24 +1,24 @@
 @echo off
 
-REM  The directory where Subsonic will create files. Make sure it is writable.
-set SUBSONIC_HOME=c:\subsonic
+REM  The directory where Supersonic will create files. Make sure it is writable.
+set SUPERSONIC_HOME=c:\supersonic
 
-REM  The host name or IP address on which to bind Subsonic. Only relevant if you have
-REM  multiple network interfaces and want to make Subsonic available on only one of them.
-REM  The default value 0.0.0.0 will bind Subsonic to all available network interfaces.
-set SUBSONIC_HOST=0.0.0.0
+REM  The host name or IP address on which to bind Supersonic. Only relevant if you have
+REM  multiple network interfaces and want to make Supersonic available on only one of them.
+REM  The default value 0.0.0.0 will bind Supersonic to all available network interfaces.
+set SUPERSONIC_HOST=0.0.0.0
 
-REM  The port on which Subsonic will listen for incoming HTTP traffic.
-set SUBSONIC_PORT=4040
+REM  The port on which Supersonic will listen for incoming HTTP traffic.
+set SUPERSONIC_PORT=4040
 
-REM  The port on which Subsonic will listen for incoming HTTPS traffic (0 to disable).
-set SUBSONIC_HTTPS_PORT=0
+REM  The port on which Supersonic will listen for incoming HTTPS traffic (0 to disable).
+set SUPERSONIC_HTTPS_PORT=0
 
-REM  The context path (i.e., the last part of the Subsonic URL).  Typically "/" or "/subsonic".
-set SUBSONIC_CONTEXT_PATH=/
+REM  The context path (i.e., the last part of the Supersonic URL).  Typically "/" or "/supersonic".
+set SUPERSONIC_CONTEXT_PATH=/
 
 REM  The memory limit (max Java heap size) in megabytes.
 set MAX_MEMORY=100
 
-java -Xmx%MAX_MEMORY%m  -Dsubsonic.home=%SUBSONIC_HOME% -Dsubsonic.host=%SUBSONIC_HOST% -Dsubsonic.port=%SUBSONIC_PORT%  -Dsubsonic.httpsPort=%SUBSONIC_HTTPS_PORT% -Dsubsonic.contextPath=%SUBSONIC_CONTEXT_PATH% -jar subsonic-booter-jar-with-dependencies.jar
+java -Xmx%MAX_MEMORY%m  -Dsubsonic.home=%SUPERSONIC_HOME% -Dsubsonic.host=%SUPERSONIC_HOST% -Dsubsonic.port=%SUPERSONIC_PORT%  -Dsubsonic.httpsPort=%SUPERSONIC_HTTPS_PORT% -Dsubsonic.contextPath=%SUPERSONIC_CONTEXT_PATH% -jar subsonic-booter-jar-with-dependencies.jar
 
--- subsonic-booter/src/main/script/subsonic.sh
+++ subsonic-booter/src/main/script/subsonic.sh
@@ -1,49 +1,49 @@
 #!/bin/sh
 
 ###################################################################################
-# Shell script for starting Subsonic.  See http://subsonic.org.
+# Shell script for starting Supersonic.  See https://github.com/Mach5/supersonic
 #
-# Author: Sindre Mehus
+# Author: Timo Reimann (devel@foo-lounge.de)
 ###################################################################################
 
-SUBSONIC_HOME=/var/subsonic
-SUBSONIC_HOST=0.0.0.0
-SUBSONIC_PORT=4040
-SUBSONIC_HTTPS_PORT=0
-SUBSONIC_CONTEXT_PATH=/
-SUBSONIC_MAX_MEMORY=100
-SUBSONIC_PIDFILE=
-SUBSONIC_DEFAULT_MUSIC_FOLDER=/var/music
-SUBSONIC_DEFAULT_PODCAST_FOLDER=/var/music/Podcast
-SUBSONIC_DEFAULT_PLAYLIST_FOLDER=/var/playlists
+SUPERSONIC_HOME=/var/supersonic
+SUPERSONIC_HOST=0.0.0.0
+SUPERSONIC_PORT=4040
+SUPERSONIC_HTTPS_PORT=0
+SUPERSONIC_CONTEXT_PATH=/
+SUPERSONIC_MAX_MEMORY=100
+SUPERSONIC_PIDFILE=
+SUPERSONIC_DEFAULT_MUSIC_FOLDER=/var/music
+SUPERSONIC_DEFAULT_PODCAST_FOLDER=/var/music/Podcast
+SUPERSONIC_DEFAULT_PLAYLIST_FOLDER=/var/playlists
 
 quiet=0
 
 usage() {
     echo "Usage: subsonic.sh [options]"
     echo "  --help               This small usage guide."
-    echo "  --home=DIR           The directory where Subsonic will create files."
-    echo "                       Make sure it is writable. Default: /var/subsonic"
-    echo "  --host=HOST          The host name or IP address on which to bind Subsonic."
+    echo "  --home=DIR           The directory where Supersonic will create files."
+    echo "                       Make sure it is writable. Default: /var/supersonic"
+    echo "  --host=HOST          The host name or IP address on which to bind Supersonic."
     echo "                       Only relevant if you have multiple network interfaces and want"
-    echo "                       to make Subsonic available on only one of them. The default value"
-    echo "                       will bind Subsonic to all available network interfaces. Default: 0.0.0.0"
-    echo "  --port=PORT          The port on which Subsonic will listen for"
+    echo "                       to make Supersonic available on only one of them. The default value"
+    echo "                       will bind Supersonic to all available network interfaces. Default: 0.0.0.0"
+    echo "  --port=PORT          The port on which Supersonic will listen for"
     echo "                       incoming HTTP traffic. Default: 4040"
-    echo "  --https-port=PORT    The port on which Subsonic will listen for"
+    echo "  --https-port=PORT    The port on which Supersonic will listen for"
     echo "                       incoming HTTPS traffic. Default: 0 (disabled)"
-    echo "  --context-path=PATH  The context path, i.e., the last part of the Subsonic"
-    echo "                       URL. Typically '/' or '/subsonic'. Default '/'"
+    echo "  --context-path=PATH  The context path, i.e., the last part of the Supersonic"
+    echo "                       URL. Typically '/' or '/supersonic'. Default '/'"
     echo "  --max-memory=MB      The memory limit (max Java heap size) in megabytes."
     echo "                       Default: 100"
     echo "  --pidfile=PIDFILE    Write PID to this file. Default not created."
     echo "  --quiet              Don't print anything to standard out. Default false."
-    echo "  --default-music-folder=DIR    Configure Subsonic to use this folder for music.  This option "
-    echo "                                only has effect the first time Subsonic is started. Default '/var/music'"
-    echo "  --default-podcast-folder=DIR  Configure Subsonic to use this folder for Podcasts.  This option "
-    echo "                                only has effect the first time Subsonic is started. Default '/var/music/Podcast'"
-    echo "  --default-playlist-folder=DIR Configure Subsonic to use this folder for playlists.  This option "
-    echo "                                only has effect the first time Subsonic is started. Default '/var/playlists'"
+    echo "  --default-music-folder=DIR    Configure Supersonic to use this folder for music.  This option "
+    echo "                                only has effect the first time Supersonic is started. Default '/var/music'"
+    echo "  --default-podcast-folder=DIR  Configure Supersonic to use this folder for Podcasts.  This option "
+    echo "                                only has effect the first time Supersonic is started. Default '/var/music/Podcast'"
+    echo "  --default-playlist-folder=DIR Configure Supersonic to use this folder for playlists.  This option "
+    echo "                                only has effect the first time Supersonic is started. Default '/var/playlists'"
     exit 1
 }
 
@@ -54,37 +54,37 @@ while [ $# -ge 1 ]; do
             usage
             ;;
         --home=?*)
-            SUBSONIC_HOME=${1#--home=}
+            SUPERSONIC_HOME=${1#--home=}
             ;;
         --host=?*)
-            SUBSONIC_HOST=${1#--host=}
+            SUPERSONIC_HOST=${1#--host=}
             ;;
         --port=?*)
-            SUBSONIC_PORT=${1#--port=}
+            SUPERSONIC_PORT=${1#--port=}
             ;;
         --https-port=?*)
-            SUBSONIC_HTTPS_PORT=${1#--https-port=}
+            SUPERSONIC_HTTPS_PORT=${1#--https-port=}
             ;;
         --context-path=?*)
-            SUBSONIC_CONTEXT_PATH=${1#--context-path=}
+            SUPERSONIC_CONTEXT_PATH=${1#--context-path=}
             ;;
         --max-memory=?*)
-            SUBSONIC_MAX_MEMORY=${1#--max-memory=}
+            SUPERSONIC_MAX_MEMORY=${1#--max-memory=}
             ;;
         --pidfile=?*)
-            SUBSONIC_PIDFILE=${1#--pidfile=}
+            SUPERSONIC_PIDFILE=${1#--pidfile=}
             ;;
         --quiet)
             quiet=1
             ;;
         --default-music-folder=?*)
-            SUBSONIC_DEFAULT_MUSIC_FOLDER=${1#--default-music-folder=}
+            SUPERSONIC_DEFAULT_MUSIC_FOLDER=${1#--default-music-folder=}
             ;;
         --default-podcast-folder=?*)
-            SUBSONIC_DEFAULT_PODCAST_FOLDER=${1#--default-podcast-folder=}
+            SUPERSONIC_DEFAULT_PODCAST_FOLDER=${1#--default-podcast-folder=}
             ;;
         --default-playlist-folder=?*)
-            SUBSONIC_DEFAULT_PLAYLIST_FOLDER=${1#--default-playlist-folder=}
+            SUPERSONIC_DEFAULT_PLAYLIST_FOLDER=${1#--default-playlist-folder=}
             ;;
         *)
             usage
@@ -100,9 +100,9 @@ if [ -e "${JAVA_HOME}" ]
     JAVA=${JAVA_HOME}/bin/java
 fi
 
-# Create Subsonic home directory.
-mkdir -p ${SUBSONIC_HOME}
-LOG=${SUBSONIC_HOME}/subsonic_sh.log
+# Create Supersonic home directory.
+mkdir -p ${SUPERSONIC_HOME}
+LOG=${SUPERSONIC_HOME}/supersonic_sh.log
 rm -f ${LOG}
 
 cd $(dirname $0)
@@ -110,25 +110,25 @@ if [ -L $0 ] && ([ -e /bin/readlink ] || [ -e /usr/bin/readlink ]); then
     cd $(dirname $(readlink $0))
 fi
 
-${JAVA} -Xmx${SUBSONIC_MAX_MEMORY}m \
-  -Dsubsonic.home=${SUBSONIC_HOME} \
-  -Dsubsonic.host=${SUBSONIC_HOST} \
-  -Dsubsonic.port=${SUBSONIC_PORT} \
-  -Dsubsonic.httpsPort=${SUBSONIC_HTTPS_PORT} \
-  -Dsubsonic.contextPath=${SUBSONIC_CONTEXT_PATH} \
-  -Dsubsonic.defaultMusicFolder=${SUBSONIC_DEFAULT_MUSIC_FOLDER} \
-  -Dsubsonic.defaultPodcastFolder=${SUBSONIC_DEFAULT_PODCAST_FOLDER} \
-  -Dsubsonic.defaultPlaylistFolder=${SUBSONIC_DEFAULT_PLAYLIST_FOLDER} \
+${JAVA} -Xmx${SUPERSONIC_MAX_MEMORY}m \
+  -Dsubsonic.home=${SUPERSONIC_HOME} \
+  -Dsubsonic.host=${SUPERSONIC_HOST} \
+  -Dsubsonic.port=${SUPERSONIC_PORT} \
+  -Dsubsonic.httpsPort=${SUPERSONIC_HTTPS_PORT} \
+  -Dsubsonic.contextPath=${SUPERSONIC_CONTEXT_PATH} \
+  -Dsubsonic.defaultMusicFolder=${SUPERSONIC_DEFAULT_MUSIC_FOLDER} \
+  -Dsubsonic.defaultPodcastFolder=${SUPERSONIC_DEFAULT_PODCAST_FOLDER} \
+  -Dsubsonic.defaultPlaylistFolder=${SUPERSONIC_DEFAULT_PLAYLIST_FOLDER} \
   -Djava.awt.headless=true \
   -verbose:gc \
   -jar subsonic-booter-jar-with-dependencies.jar > ${LOG} 2>&1 &
 
 # Write pid to pidfile if it is defined.
-if [ $SUBSONIC_PIDFILE ]; then
-    echo $! > ${SUBSONIC_PIDFILE}
+if [ $SUPERSONIC_PIDFILE ]; then
+    echo $! > ${SUPERSONIC_PIDFILE}
 fi
 
 if [ $quiet = 0 ]; then
-    echo Started Subsonic [PID $!, ${LOG}]
+    echo Started Supersonic [PID $!, ${LOG}]
 fi
 
