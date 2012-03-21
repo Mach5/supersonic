b5694c88835107d4d3efc2c5d4a229319bcfd37b
sindre_mehus <sindre_mehus@24148959-8e0e-0410-9c20-a6ce11f09cd5>
2012-03-21 14:17:11 +0000

    Optimize statistics.

--- subsonic-main/src/main/java/net/sourceforge/subsonic/dao/MediaFileDao.java
+++ subsonic-main/src/main/java/net/sourceforge/subsonic/dao/MediaFileDao.java
@@ -214,9 +214,9 @@ public class MediaFileDao extends AbstractDao {
      * @return Media library statistics.
      */
     public MediaLibraryStatistics getStatistics() {
-        int artistCount = queryForInt("select count(distinct artist) from media_file where present", 0);
-        int albumCount = queryForInt("select count(distinct album) from media_file where present", 0);
-        int songCount = queryForInt("select count(id) from media_file where type in (?, ?) and present", 0, VIDEO.name(), AUDIO.name());
+        int artistCount = queryForInt("select count(1) from artist where present", 0);
+        int albumCount = queryForInt("select count(1) from album where present", 0);
+        int songCount = queryForInt("select count(1) from media_file where type in (?, ?) and present", 0, VIDEO.name(), AUDIO.name());
         long totalLengthInBytes = queryForLong("select sum(file_size) from media_file where present", 0L);
         long totalDurationInSeconds = queryForLong("select sum(duration_seconds) from media_file where present", 0L);
 
