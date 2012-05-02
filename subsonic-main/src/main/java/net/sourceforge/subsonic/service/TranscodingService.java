7cbc2b98940d98a8648af9692cd1b6fa7c6b71ee
Der-Jan <de@r-jan.de>
2012-05-02 21:39:29 +0200

    Switch transcoding bitrate to 320kbps

--- subsonic-main/src/main/java/net/sourceforge/subsonic/service/TranscodingService.java
+++ subsonic-main/src/main/java/net/sourceforge/subsonic/service/TranscodingService.java
@@ -197,7 +197,7 @@ public class TranscodingService {
         if (transcoding != null) {
             parameters.setTranscoding(transcoding);
             if (maxBitRate == null) {
-                maxBitRate = mediaFile.isVideo() ? VideoPlayerController.DEFAULT_BIT_RATE : 128;
+                maxBitRate = mediaFile.isVideo() ? VideoPlayerController.DEFAULT_BIT_RATE : 320;
             }
         } else if (maxBitRate != null) {
             boolean supported = isDownsamplingSupported(mediaFile);
