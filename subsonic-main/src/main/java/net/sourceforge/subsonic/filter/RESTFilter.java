af1b96fa917eb213b56393d2031e200dbf5de77b
Sindre Mehus <sindre@activeobjects.no>
2012-08-02 17:33:44 +0000

    Added RESTFilter for intercepting exception in RESTController.
    git-svn-id: svn://svn.code.sf.net/p/subsonic/code/trunk@3076 104adb81-38d2-4886-885e-82898e39ce25

---
+++ subsonic-main/src/main/java/net/sourceforge/subsonic/filter/RESTFilter.java
@@ -0,0 +1,75 @@
+/*
+ This file is part of Subsonic.
+
+ Subsonic is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ Subsonic is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with Subsonic.  If not, see <http://www.gnu.org/licenses/>.
+
+ Copyright 2009 (C) Sindre Mehus
+ */
+package net.sourceforge.subsonic.filter;
+
+import net.sourceforge.subsonic.controller.RESTController;
+import org.springframework.web.bind.ServletRequestBindingException;
+import org.springframework.web.util.NestedServletException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+import static net.sourceforge.subsonic.controller.RESTController.ErrorCode.GENERIC;
+import static net.sourceforge.subsonic.controller.RESTController.ErrorCode.MISSING_PARAMETER;
+
+/**
+ * Intercepts exceptions thrown by RESTController.
+ *
+ * @author Sindre Mehus
+ * @version $Revision: 1.1 $ $Date: 2006/03/01 16:58:08 $
+ */
+public class RESTFilter implements Filter {
+
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+        try {
+            chain.doFilter(req, res);
+        } catch (Throwable x) {
+            handleException(x, (HttpServletRequest) req, (HttpServletResponse) res);
+        }
+    }
+
+    private void handleException(Throwable x, HttpServletRequest request, HttpServletResponse response) throws IOException {
+        if (x instanceof NestedServletException) {
+            x = x.getCause();
+        }
+
+        RESTController.ErrorCode code = (x instanceof ServletRequestBindingException) ? MISSING_PARAMETER : GENERIC;
+        RESTController.error(request, response, code, getErrorMessage(x));
+    }
+
+    private String getErrorMessage(Throwable x) {
+        if (x.getMessage() != null) {
+            return x.getMessage();
+        }
+        return x.getClass().getSimpleName();
+    }
+
+    public void init(FilterConfig filterConfig) {
+    }
+
+    public void destroy() {
+    }
+}
